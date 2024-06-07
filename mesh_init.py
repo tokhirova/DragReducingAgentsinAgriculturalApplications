b6fb72a2b3d5880dffd538b7799fa0d5238235a5
jea <as-alexandro@web.de>
2024-06-07 09:38:15 +0200

    added cylinder

--- mesh_init.py
+++ mesh_init.py
@@ -15,9 +15,10 @@ def create_mesh(gdim):
     model_rank = 0
     if mesh_comm.rank == model_rank:
         rectangle = gmsh.model.occ.addRectangle(0, 0, 0, L, H, tag=1)
+        obstacle = gmsh.model.occ.addDisk(c_x, c_y, 0, r, r)
 
     if mesh_comm.rank == model_rank:
-        fluid = rectangle
+        fluid = gmsh.model.occ.cut([(gdim, rectangle)], [(gdim, obstacle)])
         gmsh.model.occ.synchronize()
 
     fluid_marker = 1
@@ -27,8 +28,8 @@ def create_mesh(gdim):
         gmsh.model.addPhysicalGroup(volumes[0][0], [volumes[0][1]], fluid_marker)
         gmsh.model.setPhysicalName(volumes[0][0], fluid_marker, "Fluid")
 
-    inlet_marker, outlet_marker, wall_marker = 2, 3, 4
-    inflow, outflow, walls = [], [], []
+    inlet_marker, outlet_marker, wall_marker, obstacle_marker = 2, 3, 4, 5
+    inflow, outflow, walls, obstacle = [], [], [], []
     if mesh_comm.rank == model_rank:
         boundaries = gmsh.model.getBoundary(volumes, oriented=False)
         for boundary in boundaries:
@@ -39,6 +40,8 @@ def create_mesh(gdim):
                 outflow.append(boundary[1])
             elif np.allclose(center_of_mass, [L / 2, H, 0]) or np.allclose(center_of_mass, [L / 2, 0, 0]):
                 walls.append(boundary[1])
+            else:
+                obstacle.append(boundary[1])
 
         gmsh.model.addPhysicalGroup(1, walls, wall_marker)
         gmsh.model.setPhysicalName(1, wall_marker, "Walls")
@@ -46,6 +49,8 @@ def create_mesh(gdim):
         gmsh.model.setPhysicalName(1, inlet_marker, "Inlet")
         gmsh.model.addPhysicalGroup(1, outflow, outlet_marker)
         gmsh.model.setPhysicalName(1, outlet_marker, "Outlet")
+        gmsh.model.addPhysicalGroup(1, obstacle, obstacle_marker)
+        gmsh.model.setPhysicalName(1, obstacle_marker, "Obstacle")
 
     # Create distance field from obstacle.
     # Add threshold of mesh sizes based on the distance field
@@ -78,4 +83,4 @@ def create_mesh(gdim):
 
     mesh, _, ft = gmshio.model_to_mesh(gmsh.model, mesh_comm, model_rank, gdim=gdim)
     ft.name = "Facet markers"
-    return mesh, ft, inlet_marker, wall_marker, outlet_marker
+    return mesh, ft, inlet_marker, wall_marker, outlet_marker, obstacle_marker
